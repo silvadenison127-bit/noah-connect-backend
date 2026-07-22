f90194c0c6941e5a2366634a22db8399d4a946ba
silvadenison127-bit <silvadenison127@gmail.com>
2026-07-21 23:44:14 -0300

    Delete src directory

--- src/config/db.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-require('dotenv').config();
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL?.includes('railway') ? { rejectUnauthorized: false } : false
-});
-
-module.exports = pool;
